8f0f6c56b7814bedc431264b7f45095c93f8460f
Dave Boyle <spektraldevelopment@gmail.com>
2019-06-24 19:34:28 -0400

    [master] - Moved auto generated nuxt files to root.

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Hello: {{this.title}}</div>
+</template>
+
+<script>
+
+import Vuex, { mapState } from 'vuex';
+import Vue from 'vue';
+
+import store from '../store/store';
+
+export default {
+  computed: {
+    ...mapState({
+      title: state => state.title
+    })
+  },
+  store: store
+}
+</script>
+
+<style>
+
+</style>
